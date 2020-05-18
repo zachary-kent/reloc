15526ec5d3ab67939c74d78c3383ac8463018eef
Ralf Jung <jung@mpi-sws.org>
2020-05-18 16:27:48 +0200

    update Iris

--- theories/logic/adequacy.v
+++ theories/logic/adequacy.v
@@ -28,7 +28,7 @@ Lemma refines_adequate Σ `{relocPreG Σ}
             ∧ P v v').
 Proof.
   intros HA Hlog.
-  eapply (heap_adequacy Σ _); iIntros (Hinv).
+  eapply (heap_adequacy Σ _); iIntros (Hinv) "_".
   iMod (own_alloc (● (to_tpool [e'], to_gen_heap (heap σ))
     ⋅ ◯ ((to_tpool [e'] : tpoolUR, ∅) : cfgUR)))
     as (γc) "[Hcfg1 Hcfg2]".
